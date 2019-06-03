a536c3f32e6d79fa373450f547474e77bbbfb65e
William <williamd8323@afsenyc.org>
2019-06-03 10:04:01 -0400

    idk

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -14,9 +14,17 @@ export default class Comments extends React.Component {
         )
     }
 
-    handleSubmit(e) {
+    handleSubmit (e) {
         e.preventDefault(); // used to prevent defualt aciton that would otherwise occur if prevent wasnt added
-        console.log("submitting form...");
+        // console.log("submitting form...");
+        // console.log(this.refs);
+        //es6 destructuring
+        const { postid } = this.props.params;
+        // const postId = this.props.params.postid;
+        const author = this.refs.author.value;
+        const comment = this.refs.comment.value;
+        // console.log(postId, comment, author);
+        this.props.addComment(postid, author, comment);
     }
 
     render() {
@@ -24,7 +32,8 @@ export default class Comments extends React.Component {
         return (
          <div className="comments">
              {this.props.postComments.map(this.renderComments)}
-             <form ref="commentForm" className='comment-form' onSubmit={this.handleSubmit}>
+             {/* remember to .bind(this) to be able to this.refs */}
+             <form ref="commentForm" className='comment-form' onSubmit={this.handleSubmit.bind(this)}> 
                  <input type="text" ref="author" placeholder="author"/>
                  <input type="text" ref="comment" placeholder="comment"/>
                  <input type="submit" hidden={true}/>
